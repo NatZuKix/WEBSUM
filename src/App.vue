eaf1129e67292529974dd23a8b757247b0e35723
Nutthanon <Nutthanon.than@kmutt.ac.th>
2024-11-12 03:20:05 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-screen h-screen bg-gradient-to-b from-[#9bbdd6] via-[#b6cee2] to-[#bdd3e4]">
+   <div class="flex justify-center pt-10">
+    <img src="../public/pic/logo.png" class="w-5/6 "/>
+   </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
